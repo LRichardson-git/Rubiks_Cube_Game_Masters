c288459103cc856e32c568a0eb5a574a31479910
liammmmmmm96 <liam-richo12@hotmail.co.uk>
2021-10-31 01:29:28 +0100

    Revert "Merge branch 'master' into test"
    This reverts commit dfaabb3a190425bac70bd52fab805ce584ba7e42, reversing
    changes made to 0e5b66833c2b96a1b15aca8d7c2a3f80b5296011.

--- Source/Rubiks_Cube_Game/Rubiks_Cube_Game.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Rubiks_Cube_Game.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Rubiks_Cube_Game, "Rubiks_Cube_Game" );
